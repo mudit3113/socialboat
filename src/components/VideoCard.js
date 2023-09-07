fc8f3ce4783ebd5e31719fa361fad2b173177f99
mudit3113 <116980345+mudit3113@users.noreply.github.com>
2023-09-07 20:42:41 +0530

    "second commit"

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -2,7 +2,6 @@ import React from 'react'
 import './display.css'
 
 function VideoCard({ videoContent, queryHeading, tags }) {
-  console.log('ss', queryHeading)
   return (
     <div className="video-card">
       <video src={videoContent} controls></video>
